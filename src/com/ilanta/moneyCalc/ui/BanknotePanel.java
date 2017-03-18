a661ecfe7163ca4f3ad5c075690511dcb11a50e8
Birviz Kozak <birvizkozak@gmail.com>
2017-03-18 13:06:44 +0200

    .

--- src/com/ilanta/moneyCalc/ui/BanknotePanel.java
+++ src/com/ilanta/moneyCalc/ui/BanknotePanel.java
@@ -6,18 +6,18 @@ import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
-public class BanknotePanel extends JPanel {
+class BanknotePanel extends JPanel {
     private JTextField textField = new JTextField("0",10);
     private JLabel label = new JLabel("0 грн.");
     private int quantity;
     private int result;
     private final int nominal;
 
-    public int getResult() {
+    int getResult() {
         return result;
     }
 
-    public void setQuantity(int quantity) {
+    void setQuantity(int quantity) {
         this.quantity = quantity;
         calc();
         if(!String.valueOf(quantity).equals(textField.getText())) {
@@ -56,10 +56,9 @@ public class BanknotePanel extends JPanel {
             @Override
             public void focusLost(FocusEvent e) {
                 try {
-                    quantity = Integer.valueOf(textField.getText());
+                    setQuantity(Integer.valueOf(textField.getText()));
                 } catch (NumberFormatException except) {
-                    quantity = 0;
-                    textField.setText("0");
+                    setQuantity(0);
                 }
                 calc();
             }
