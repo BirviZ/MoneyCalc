bb62e3a809b04b00e1599a248cd1e05e8607d093
Birviz Kozak <birvizkozak@gmail.com>
2017-03-20 15:41:30 +0200

    Add read method

--- src/com/ilanta/moneyCalc/savings/FileIO.java
+++ src/com/ilanta/moneyCalc/savings/FileIO.java
@@ -1,11 +1,26 @@
 package com.ilanta.moneyCalc.savings;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 class FileIO {
     private static final String fileName = ".money";
 
-    protected boolean isExists(){
+    protected static boolean isExists(){
         return new File(fileName).exists();
     }
+    
+    protected static String readFile() throws FileNotFoundException {
+    	File file = new File(fileName);
+    	Scanner sc = new Scanner(file);
+    	String result = sc.nextLine();
+    	sc.close();
+    	return result;
+    }
+    
+    protected static void writeFile() {
+    	
+    }
+    
 }
--- src/com/ilanta/moneyCalc/savings/Save.java
+++ src/com/ilanta/moneyCalc/savings/Save.java
@@ -1,5 +1,7 @@
 package com.ilanta.moneyCalc.savings;
 
+import java.io.FileNotFoundException;
+
 public class Save extends FileIO {
 
     public static void write(int[] values) {
@@ -8,16 +10,41 @@ public class Save extends FileIO {
     }
 
     public static int[] read() {
-        int[] v = new int[9];
-
-        for(int i = 0; i < 9; i++) {
-            v[i] = (int)(Math.random()*100);
+        if(isExists()) {
+        	String str;
+        	try {
+				str = readFile();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				return zero();
+			}
+        	
+        	String[] array = str.split(",");
+        	int[] result = new int[array.length];
+        	for(int i = 0; i < array.length; i++) {
+        		try {
+        			result[i] = Integer.valueOf(array[i]);
+        		} catch (NumberFormatException e) {
+					result[i] = 0;
+				}
+        	}
+        	return result;
+        } else {
+        	return zero();
         }
-
-        return v;
     }
 
     public static void delete() {
 
+    }
+    
+    private static int[] zero() {
+    	int[] v = new int[9];
+
+        for(int i = 0; i < 9; i++) {
+            v[i] = 0;
+        }
+
+        return v;
     }
 }
